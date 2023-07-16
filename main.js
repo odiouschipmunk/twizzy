aff8e31f69ba5f573372c18a0cf63bc4d9f05f7c
odiouschipmunk <odiouschipmunk@gmail.com>
2023-07-16 17:22:36 -0400

    krank

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 var express = require('express')
 var app = express()
 var server = app.listen(3000, listen)
+app.set('view engine', 'ejs')
 
 function listen() {
   var host = server.address().address
   var port = server.address().port
-  console.log('Example app listening at http://' + host + ':' + port)
 }
 
 app.use(express.static('public'))
@@ -13,7 +13,7 @@ app.use(express.static('public'))
 var io = require('socket.io')(server)
 
 app.get("/", (req, res)=>{
-    res.send("twizz")
+    res.render("views/head");
 })
 io.sockets.on(
   'connection',
